c54c24bf721eb5af048a4014e51b98679fc0a024
Y. Violet Guo <yueguo598@gmail.com>
2019-11-12 21:53:28 -0500

    RC commit, small debyugging into indexing

--- src/tree.py
+++ src/tree.py
@@ -11,6 +11,7 @@ from src import commons as dut
 class DecisionTreeCatgorical:
 
     def fit(self, X, y, min_leaf=5):
+        print("np.array(np.arange(len(y)))", np.array(np.arange(len(y))))
         self.dtree = Node(X, y, np.array(np.arange(len(y))), min_leaf)
         return self
 
@@ -21,12 +22,14 @@ class DecisionTreeCatgorical:
 class Node:
 
     def __init__(self, x, y, idxs, min_leaf_count=5):
+        assert(x.shape[1]==4)
+        print("x.shape[1]", x.shape[1])
         self.x = x
         self.y = y
         self.idxs = idxs
         self.min_leaf_count = min_leaf_count
         self.row_count = len(idxs)
-        self.col_count = x.shape[1]
+        self.col_count = x.shape[1] - 1
         self.val = np.mean(y[idxs])
         self.score = float('inf')
         self.find_varsplit()
@@ -39,29 +42,41 @@ class Node:
             return
 
         selected_x = self.x.values[self.idxs, self.var_idx]
-        lhs = np.nonzero(selected_x <= self.split)[0]
-        rhs = np.nonzero(selected_x > self.split)[0]
-        self.lhs = Node(self.x, self.y, self.idxs[lhs])
-        self.rhs = Node(self.x, self.y, self.idxs[rhs])
+        # lhs = np.nonzero(selected_x <= self.split)[0]
+        # rhs = np.nonzero(selected_x > self.split)[0]
+        self.lhs = Node(self.x, self.y, self.best_lhs_indices)
+        self.rhs = Node(self.x, self.y, self.best_rhs_indices)
 
     def cat_split(self, var_idx):
-        col = self.x.iloc[self.idxs, var_idx]
-        unique = col.unique()
-        res = list()
-        for threshold in unique:
-            left, right = list(), list()
-            print("type col", type(col))
-            for index, col_val in col.items():
-                if col_val < threshold:
-                    # only append index of that row, not making a copy
-                    left.append(index)
-                else:
-                    right.append(index)
-            res.append((left, right, threshold))
-        return res
+        assert(var_idx < 4)
+
+        try:
+            col = self.x.iloc[self.idxs, var_idx]
+
+            unique = col.unique()
+            res = list()
+            for threshold in unique:
+                left, right = list(), list()
+                # print("type col", type(col))
+                for index, col_val in col.items():
+                    if col_val < threshold:
+                        # only append index of that row, not making a copy
+                        left.append(index)
+                    else:
+                        right.append(index)
+                res.append((left, right, threshold))
+                return res
+
+        except Exception as e:
+
+
+            print(e)
+            print("self.idxs", self.idxs)
+            print("var_idx", var_idx)
 
     def find_better_split(self, var_idx):
-        x_col = self.x.iloc[self.idxs, var_idx]
+
+        # x_col = self.x.iloc[self.idxs, var_idx]
         # TODO: don't use flower name as the target var??
         splits = self.cat_split(var_idx)
 
@@ -87,11 +102,10 @@ class Node:
             curr_score = self.find_score(split)
             if curr_score < self.score:
                 self.var_idx = var_idx
+                # NOTE: this is the line that makes it the global
                 self.score = curr_score
                 self.split = split
-                best_lhs, best_rhs = lhs, rhs
-        # TODO: this only returns the best lhs, rhs of each col feature, not the global one
-        return best_lhs, best_rhs
+                self.best_lhs_indices, self.best_rhs_indices = lhs, rhs
 
 
 
@@ -106,9 +120,9 @@ class Node:
             total_rows = subtree.shape[0]
             if len(split[0]) > 0:
                 for idx in subtree:
-                    print("idx", idx)
-                    break
-                    if self.y.iloc[idx] == category:
+                    # print("idx", idx)
+                    # print("sleg y", self.y)
+                    if self.y[idx] == category:
                         sum_cat += 1
                 pk.append(sum_cat / len(subtree))  # Find proportion in each class
             score += (1 - sum([p ** 2 for p in pk])) * (len(subtree) / total_rows)
@@ -138,20 +152,21 @@ class Node:
         return node.predict_col_helper(xi)
 
 
-filename = "../data/iris_data.csv"
+filename = "../data/iris_test.csv"
 df = cat_util.read_pd(filename)
 train_df, test_df = cat_util.split_train_test(df)
 print("df shape", df.shape)
 # NOTE!!: this must be done, otherwise some strange indexing error in pandas
-test_df = df[0:100]
 X = train_df[["sepal_length","sepal_width", "petal_length", "petal_width"]]
 y = train_df["species"]
 
+print(X.shape)
+print(X.iloc[5])
 start_time = time.time()
 
-regressor = DecisionTreeCatgorical().fit(X, y)
-X = test_df[0:50][["sepal_length","sepal_width", "petal_length", "petal_width"]]
-actual = test_df[0:50]["species"]
+# regressor = DecisionTreeCatgorical().fit(X, y)
+X = test_df[["sepal_length","sepal_width", "petal_length", "petal_width"]]
+actual = test_df["species"]
 preds = regressor.predict(X)
 print("--- %s seconds ---" % (time.time() - start_time))
 
